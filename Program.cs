3fdce34996e0359bac10fe5d4a95baedc0a5415e
souzet tadros <souzettadros@gmail.com>
2020-11-09 20:44:33 +0100

    2

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace RowCharCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ange ett filnamn:  ");
+            Console.WriteLine("Ange ett filnamn:  "); // ‪C:\Users\souzet\Documents\exempelfil-sv.txt
             string fileName = Console.ReadLine();
             Console.WriteLine("Öppnar: {0}",fileName );
             // Från: https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/
@@ -19,23 +19,23 @@ namespace RowCharCount
             using (StreamReader file = new StreamReader(fileName))
             {
                 int counter = 0;
-                int charnum = 0;
-                int wordnum = 0;
+                //int charnum = 0;
+                //int wordnum = 0;
                 string ln;
 
                 while ((ln = file.ReadLine()) != null)
                 {
-                    //Console.WriteLine(ln);
+                    Console.WriteLine(ln);
                     counter++;
-                    charnum = charnum + ln.Length; // Fixa radslutstecken...
-                    var punctuation = ln.Where(Char.IsPunctuation).Distinct().ToArray();
-                    var words = ln.Split().Select(x => x.Trim(punctuation));
-                    wordnum = wordnum + words.Count();
+                    //charnum = charnum + ln.Length; // Fixa radslutstecken...
+                    //var punctuation = ln.Where(Char.IsPunctuation).Distinct().ToArray();
+                    //var words = ln.Split().Select(x => x.Trim(punctuation));
+                    //wordnum = wordnum + words.Count();
                 }
                 file.Close();
                 Console.WriteLine($"File has {counter} lines.");
-                Console.WriteLine($"File has {wordnum} words.");
-                Console.WriteLine($"File has {charnum} chars.");
+                //Console.WriteLine($"File has {wordnum} words.");
+                //Console.WriteLine($"File has {charnum} chars.");
 
             }
         }
